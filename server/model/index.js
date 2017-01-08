757e8062db764874d822e75b73005a248be8ff1c
filip <filip@brainhub.pl>
2017-01-08 23:18:44 +0100

    add callbacks to database

--- server/model/index.js
+++ server/model/index.js
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose';
 
-const connection = mongoose.connect(
+const connection = (onConnectCb) => mongoose.connect(
   `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT || 27017}/${process.env.DB_NAME}`,
   {
     server: { auto_reconnect: true },
     user: process.env.DB_USER || '',
     pass: process.env.DB_PASS || ''
-  }
+  },
+  onConnectCb
 );
 
+connection.disconnect = (cb) => mongoose.connection.close(cb);
+connection.getConnection = () => mongoose.connection;
+
 process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
+  connection.disconnect(function () {
     process.exit(0);
   });
 });
