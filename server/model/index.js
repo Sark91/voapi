98799fd2208e4325503532882efd86d7d7858130
filip <filip@brainhub.pl>
2017-01-08 22:09:01 +0100

    connection fix

--- server/model/index.js
+++ server/model/index.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 
-export default mongoose.createConnection(
-  process.env.DB_HOST,
-  process.env.DB_NAME,
-  process.env.DB_PORT, {
+const connection = mongoose.connect(
+  `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT || 27017}/${process.env.DB_NAME}`,
+  {
     server: { auto_reconnect: true },
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS
+    user: process.env.DB_USER || '',
+    pass: process.env.DB_PASS || ''
   }
 );
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    process.exit(0);
+  });
+});
+
+export default connection;
