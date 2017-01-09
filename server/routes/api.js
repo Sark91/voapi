695ba4bcd94b59aebb3520e541b2df7ff2cd76bb
filip <filip@brainhub.pl>
2017-01-09 01:09:38 +0100

    add burn to api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
 import authorization from 'server/routes/authorization';
 import campaign from 'server/routes/campaign';
+import burn from 'server/routes/burn';
 import restify from 'server/routes/restify';
 
 export default (app) => {
   app.use('/api', authorization(app));
   app.post('/api/v1/Campaign', campaign());
+  app.post('/api/v1/burn/:voucherId', burn());
   restify(app);
 };
