e1bb19e793b5a33f79f6b7965658c55544a75554
filip <filip@brainhub.pl>
2017-01-09 01:08:30 +0100

    remove unused code

--- server/server.js
+++ server/server.js
@@ -12,11 +12,6 @@ appRoutes(app);
 
 const server = (serverStartCb = () => {}) => {
   const serverInstance = app.listen(process.env.SERVER_PORT, () => {
-    const { address, port } = serverInstance.address();
-
-    // eslint-disable-next-line no-console
-    // console.log(`App listening at http://${address}:${port}`);
-
     dbConnection(serverStartCb);
   });
 
